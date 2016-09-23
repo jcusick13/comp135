97ebafa4d4a1fe7372c932abe836237ed155ae0d
Jon Cusick <jonathan.cusick09@gmail.com>
2016-09-22 21:43:58 -0400

    Implemented find_neighbors

--- hw1/arrf_reader.py
+++ hw1/arrf_reader.py
@@ -62,14 +62,66 @@ class arrf:
             print i + ': ', getattr(row, i)
 
 
-def euclidean_dist(x, y, weight=1):
+def euclidean_dist(x, y, weight=1.0):
     """Calculates Euclidean distance between x and y,
     where x and y are both tuples.
     Accepts weights as floating point numbers.
     """
-    for i in range(len(x) - 1):
-        print x[i]
+    sq_diff = 0
+    for i in range(len(x)):
+        sq_diff += weight * (x[i] - y[i]) ** 2
+
+    return sq_diff ** (1 / 2.0)
 
 
-iono = arrf('ionosphere_test.arff')
-euclidean_dist(iono.data[0], iono.data[1])
+def find_neighbors(training, inst, k):
+    """Performs a linear search through the
+    list of tuples in training and returns list of
+    the k neighbors nearest to the instance tuple inst
+    using a euclidean distance search. List is orderded
+    from smallest to largest distance.
+
+    Biased for keeping first neighbor seen in case of
+    a tie in distance between two potential nearest
+    neighbors.
+    """
+
+    nn = []
+
+    # Evaluate distance of each training example
+    for exp in training:
+        dist = euclidean_dist(exp, inst)
+
+        if len(nn) < k:
+            # Naively add example to nn list with its Euclid
+            # distance added as first tuple element
+            nn.append((dist,) + exp)
+            nn.sort(key=lambda t: t[0])
+
+        else:
+            # Compare dist to training examples currently in nn
+            # starting with worst example
+            for nbr in reversed(nn):
+                if dist < nbr[0]:
+                    # Remove worst neighbor, add exp, reorder nn
+                    del nn[-1]
+                    nn.append((dist,) + exp)
+                    nn.sort(key=lambda t: t[0])
+                    break
+
+    # Remove stored euclidean distances
+    nn_cleaned = []
+    for i in nn:
+        nn_cleaned.append(i[1:])
+
+    return nn_cleaned
+
+
+if __name__ == '__main__':
+
+    # iono = arrf('ionosphere_test.arff')
+
+    train = [(1, 2), (2, 3), (10, 10), (12, 12)]
+    instance = (2, 2)
+
+    print find_neighbors(train, instance, 2)
