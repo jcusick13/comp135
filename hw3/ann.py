0abbc3ed8334a167e866a97b0fa902053da57cdf
Jon Cusick <jonathan.cusick09@gmail.com>
2016-11-12 13:29:34 -0500

    Built Node() class and corresponding tests

--- hw3/ann.py
+++ hw3/ann.py
@@ -1,27 +1,85 @@
 # ann.py
 import math
+import numpy as np
 
 
-def sigmoid(i):
-    """Calculates the sigmoid of i, returns as float.
-
-    i: float/int
+class Node():
+    """Individual node for use within a neural network.
+    Can be used anywhere in net (i.e. input, output, hidden).
     """
 
-    # Ensure float input
-    i = i / 1.0
+    def __init__(self, value=0.0, inputs=0):
+        """All nodes default to value of zero and random weight
+        uniformly sampled from range [-0.1, 0.1].
 
-    return 1.0 / (1.0 + math.exp(-i))
+        value: float, initial value of the node.
+
+        inputs: int, number of nodes from the previous layer that
+                have a connection to this node.
+        """
+        self.value = value
+        if inputs == 0:
+            self.weights = None
+        else:
+            self.weights = np.random.uniform(-0.1, 0.1, inputs)
+
+    def update_value(self, prev_values):
+        """Sets the the output value (x_i) of a node, using the
+        sigmoid function.
+
+        prev_values: list, final value (x_i) of each node
+                    in the previous layer.
+        """
+        s = self._s_i(prev_values)
+        self.value = self._sigmoid(s)
+
+    def _s_i(self, prev_values):
+        """Calculates s_i, the sum of all values (multiplied by
+        their weights) of connected nodes from the previous layer.
+
+        prev_values: list, final value (x_i) of each node
+                    in the previous layer.
+        """
+        return np.dot(self.weights, prev_values)
+
+    def _sigmoid(self, i):
+        """Calculates the sigmoid of i (x_i), returns as float.
+
+        i: float/int
+        """
+
+        # Ensure float input
+        i = i / 1.0
+        return 1.0 / (1.0 + math.exp(-i))
+
+    def _sigmoid_p(self, i):
+        """Calculates the derivative of the sigmoid
+        of i, returns as float.
+
+        i: float/int
+        """
+
+        # Ensure float input
+        i = i / 1.0
+        return i * (1.0 - i)
 
 
-def sigmoid_p(i):
-    """Calculates the derivative of the sigmoid
-    of i, returns as float.
+class Layer():
+    """Collection of nodes within one layer of a neural network."""
 
-    i: float/int
-    """
+    def __init__(self, nodes, prev=None, follow=None):
+        """Initializes array of individual nodes.
 
-    # Ensure float input
-    i = i / 1.0
+        nodes: int, the number of nodes to be contained in the
+                layer (i.e. layer width)
 
-    return i * (1.0 - i)
+        prev: Layer, the layer immediately preceeding (closer to
+              input nodes) the current layer.
+
+        follow: Layer, the layer immediately after (closer to
+                output nodes) the current layer.
+        """
+        self.prev = prev
+        self.follow = follow
+        self.nodes = np.array([Node() for x in nodes], dtype=object)
+
--- hw3/ann_test.py
+++ hw3/ann_test.py
@@ -6,24 +6,64 @@ from ann import *
 class TestAnnFunctions(unittest.TestCase):
 
     #
-    # sigmoid()
+    # Node.__init__()
+    #
+
+    def test_node_weight_init(self):
+        """Ensures weights are only initialized for a node
+        when passed in an input value.
+        """
+        n = Node(value=2.0)
+        self.assertEqual(n.weights, None)
+
+    def test_node_weight_range_min(self):
+        """Ensures random weight values are initialized
+        within the correct range.
+        """
+        n = Node(inputs=6)
+        for i in n.weights:
+            self.assertGreaterEqual(i, -0.1)
+
+    def test_node_weight_range_max(self):
+        """Ensures random weight values are initialized
+        within the correct range.
+        """
+        n = Node(inputs=3)
+        for i in n.weights:
+            self.assertLess(i, 0.1)
+
+    #
+    # Node.update_value()
+    #
+
+    def test_update_value(self):
+        """Ensures node output value is correctly calculated."""
+        n = Node(inputs=2)
+        # Override weights to static value for reproducibility
+        n.weights = [1, 1]
+        n.update_value([2, 3])
+
+        self.assertEqual(round(n.value, 3), 0.993)
+
+    #
+    # Node._sigmoid()
     #
 
     def test_sigmoid_convert_int(self):
         """Ensures output is the same regardless if input
         is given as int or float.
         """
-
-        self.assertEqual(sigmoid(5), sigmoid(5.0))
+        n = Node()
+        self.assertEqual(n._sigmoid(5), n._sigmoid(5.0))
 
     #
-    # sigmoid_p()
+    # Node._sigmoid_p()
     #
 
     def test_sigmoid_p_output(self):
         """Ensures correct output of sigmoid derivative."""
-
-        self.assertEqual(round(sigmoid_p(0.993), 3), 0.007)
+        n = Node()
+        self.assertEqual(round(n._sigmoid_p(0.993), 3), 0.007)
 
 
 if __name__ == '__main__':
