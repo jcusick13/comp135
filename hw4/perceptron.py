4a3f8f355fb94b48500e12f705ddc78acb965e0e
Jon Cusick <jonathan.cusick09@gmail.com>
2016-12-03 14:36:24 -0500

    Initial code for polynomial kernal

--- hw4/perceptron.py
+++ hw4/perceptron.py
@@ -1,23 +1,50 @@
 # perceptron.py
 import numpy as np
+import math
 
 
 class PrimalPerceptron():
     """Implementation of the Primal Perceptron algorithm
     with margins.
-
-    dim: int, dimension count of input data
     """
 
     def __init__(self, dim):
         """Creates weight vector w to match
         the input dimensionality.
+
+        dim: int, dimension count of input data
         """
         self.w = [0.0 for val in range(dim)]
+        self.margin = 0.0
 
     def __str__(self):
         """Return current list of weights for printing."""
         return str(self.w)
+
+    def calc_margin(self, train_data):
+        """Calculates seperability margin as 0.1 * the
+        average norm of training examples.
+
+        train_data: 2d-list, input training data
+        """
+        count = 0.0  # Total training examples
+        running_tally = 0.0  # Sum of all norms
+
+        # Sum norm over all examples
+        for ex in train_data:
+
+            # Calculate norm for individual example
+            ex_tally = 0.0
+            for ft in ex[:-1]:  # Ignore example label
+                ex_tally += (ft ** 2)
+            norm = math.sqrt(ex_tally)
+
+            running_tally += norm
+            count += 1.0
+
+        margin = 0.1 * (running_tally / count)
+        self.margin = margin
+        return margin
 
     def classify(self, in_vals):
         """Computes signed dot product of input
@@ -29,9 +56,9 @@ class PrimalPerceptron():
 
         # Return signed value
         if val < 0:
-            return -1
+            return -1, val
         else:
-            return 1
+            return 1, val
 
     def update(self, in_vals, y):
         """Updates weight vector w according to
@@ -42,3 +69,177 @@ class PrimalPerceptron():
         """
         for k in range(len(in_vals)):
             self.w[k] += (y * in_vals[k])
+
+    def train(self, train_data, i):
+        """Learns weight vector from training
+        examples with i iterations.
+
+        train_data: Arff class of training data
+        i: int, number of training iterations
+        """
+        # Calculate margin
+        self.calc_margin(train_data.data)
+
+        # Train
+        for iteration in range(i):
+            for ex in train_data.data:
+                # Convert tuple to list
+                ex_list = [x for x in ex[:-1]]
+                # Add constant 1 to example to account for threshold
+                example = ex_list + [1]
+                o, value = self.classify(example)
+
+                # Update weights if prediction incorrect/within margin
+                if (o * value) < self.margin:
+                    self.update(example, o)
+
+        # Return final weight vector as hypothesis
+        return self.w
+
+    def test(self, test_data):
+        """Labels examples in test_data according to current
+        weight vector w, returns test dataset accuracy.
+
+        test_data: Arff class of training data
+        """
+        correct = 0.0
+        total = 0.0
+
+        for ex in test_data.data:
+            # Convert tuple to list
+            ex_list = [x for x in ex[:-1]]
+            # Add constant 1 to example to account for threshold
+            example = ex_list + [1]
+            o, val = self.classify(example)
+
+            # Compare labels, record result
+            print 'o: %i, label: %i' % (o, int(ex[-1]))
+            if o == int(ex[-1]):
+                correct += 1.0
+
+            total += 1.0
+
+        return correct / total
+
+
+class KernelPerceptron():
+    """Implementation of Kernel Perceptron algorithm
+    with both polynomial and RBF kernels.
+    """
+
+    def __init__(self, ex_ct, poly=False, d=0, RBF=False, s=0.0):
+        """Initializes all zero alpha vector for length
+        of the input example count.
+
+        ex_ct: int, count of training examples
+        poly: bool, switch to indicate polynomial kernel
+        d: int, exponential parameter of polynomial kernel
+        RBF: bool, switch to indicated RBF kernel
+        s: float, RBF kernel parameter
+        """
+        self.alpha = [0 for x in range(ex_ct)]
+        self.poly = poly
+        self.d = d
+        self.rbf = RBF
+        self.s = s
+        self.margin = 0.0
+
+    def __str__(self):
+        """Returns alpha vector as a string for printing."""
+        return str(self.alpha)
+
+    def poly_kernel(self, u, v):
+        """Computes polynomial kernel of two vectors u and v:
+        (u * v + 1)^d
+
+        u: list/array, input vector of numeric values
+        v: list/array, input vector of numeric values (same length as u)
+        """
+        return (np.dot(u, v) + 1) ** self.d
+
+    def calc_margin(self, train_data):
+        """Calculates the seperability margin as
+        0.1 * average kernel value. Uses either polynomial
+        or RBF kernel as specified by class parameter.
+
+        train_data: 2-d list, input training data
+        """
+        # Polynomial kernel
+        if self.poly:
+            count = 0.0  # Total training examples
+            running_tally = 0.0  # Sum of all sqrt(kernel)
+
+            # Sum sqrt(kernel) over all examples
+            for ex in train_data:
+                # Calculate kernel, add to running tally
+                kernel = self.poly_kernel(ex[:-1], ex[:-1])
+                running_tally += math.sqrt(kernel)
+
+                count += 1.0
+
+            margin = 0.1 * (running_tally / count)
+            self.margin = margin
+            return margin
+
+        # RBF kernel
+        elif self.rbf:
+            return 0
+
+    def classify(self, in_vals, dataset):
+        """Computes signed classifer function of input values
+
+        in_vals: list/array, training set example to classify
+        dataset: 2-d list/array, full train/test dataset
+        """
+        # Polynomial kernel
+        if self.poly:
+            val = 0.0
+            for k in range(len(dataset)):
+                u = dataset[k][:-1]  # k-th dataset example
+                v = in_vals[:-1]  # example to classify
+
+                # (alpha_k * y_k * kernel value)
+                val += self.alpha[k] * dataset[k][-1] * self.poly_kernel(u, v)
+
+        # RBF kernel
+        if self.rbf:
+            val = 0
+
+        # Return signed value
+        if val < 0:
+            return -1, val
+        else:
+            return 1, val
+
+    def update(self, i):
+        """Updates alpha vector with
+        a_i <- a_i + 1
+
+        i: int, alpha vector element to be updated
+        """
+        self.alpha[i] += 1
+
+    def train(self, traindata, i):
+        """Learns alpha vector values from input dataset.
+
+        traindata: Arff class, training dataset
+        i: int, number of training iterations
+        """
+        # Calculate margin
+        self.calc_margin(traindata.data)
+
+        # Train
+        for iteration in range(i):
+            curr_example = 0
+            for ex in traindata.data:
+                o, val = self.classify(ex, traindata.data)
+
+                # Update alpha if prediction incorrect/within margin
+                if (o * val) < self.margin:
+                    self.update(curr_example)
+
+                # Update current example count
+                curr_example += 1
+
+        # Return final alpha vector as hypothesis
+        return self.alpha
--- hw4/perceptron_test.py
+++ hw4/perceptron_test.py
@@ -8,7 +8,7 @@ class TestPerceptronFns(unittest.TestCase):
     #
     # PrimalPerceptron.calc_margin()
     #
-    def test_margin_calculation(self):
+    def test_primal_margin_calculation(self):
         """Ensures proper margin calculation."""
         train_data = [[2, 4, 1], [3, 2, -1]]
         p = PrimalPerceptron(2)
@@ -16,7 +16,7 @@ class TestPerceptronFns(unittest.TestCase):
 
         self.assertEqual(round(margin, 4), 0.4039)
 
-    def test_set_margin(self):
+    def test_primal_set_margin(self):
         """Ensures margin attribute is updated."""
         train_data = [[2, 4, 1], [3, 2, -1]]
         p = PrimalPerceptron(2)
@@ -27,7 +27,7 @@ class TestPerceptronFns(unittest.TestCase):
     #
     # PrimalPerceptron.classify()
     #
-    def test_output_1(self):
+    def test_primal_output_1(self):
         """Ensures output label set to 1."""
         train_data = [2, 4]
         p = PrimalPerceptron(2)
@@ -35,7 +35,7 @@ class TestPerceptronFns(unittest.TestCase):
         o, value = p.classify(train_data)
         self.assertEqual(o, 1)
 
-    def test_output_0(self):
+    def test_primal_output_0(self):
         """Ensures output label set to 0."""
         train_data = [1, 3]
         p = PrimalPerceptron(2)
@@ -47,7 +47,7 @@ class TestPerceptronFns(unittest.TestCase):
     #
     # PrimalPerceptron.update()
     #
-    def test_weight_update(self):
+    def test_primal_weight_update(self):
         """Ensures weights are correctly updated after
         training example.
         """
@@ -57,6 +57,61 @@ class TestPerceptronFns(unittest.TestCase):
 
         p.update(vals, y)
         self.assertEqual(p.w, [1, 2])
+
+    #
+    # KernelPerceptron.poly_kernel()
+    #
+    def test_poly_kernel_linear_calc(self):
+        """Ensure correct output for kernel calculation."""
+        u = [3, 2, 1]
+        v = [1, 2, 3]
+
+        kp = KernelPerceptron(2, poly=True, d=1)
+        kernel = kp.poly_kernel(u, v)
+
+        self.assertEqual(kernel, 11)
+
+    def test_poly_kernel_power_calc(self):
+        """Ensure correctness when raised to a power."""
+        u = [3, 2, 1]
+        v = [1, 2, 3]
+
+        kp = KernelPerceptron(2, poly=True, d=3)
+        kernel = kp.poly_kernel(u, v)
+
+        self.assertEqual(kernel, 1331)
+
+    #
+    # KernelPerceptron.calc_margin()
+    #
+    def test_poly_kernel_margin(self):
+        """Ensure correct margin calculation."""
+        train_data = [[2, 4, 6, -1], [1, 2, 3, 1]]
+        kp = KernelPerceptron(2, poly=True, d=1)
+        margin = kp.calc_margin(train_data)
+
+        self.assertEqual(round(margin, 4), 0.5711)
+
+    def test_poly_kernel_margin_update(self):
+        """Checks that margin is updated after calculation."""
+        train_data = [[2, 4, 6, -1], [1, 2, 3, 1]]
+        kp = KernelPerceptron(2, poly=True, d=1)
+        kp.calc_margin(train_data)
+
+        self.assertEqual(round(kp.margin, 4), 0.5711)
+
+    #
+    # KernelPerceptron.classify()
+    #
+    def test_poly_kernel_classify_initial(self):
+        """Label should be 1 because of zero
+        initialized alpha vector."""
+        train_data = [[2, 4, 6, -1], [1, 2, 3, 1]]
+        example = [2, 4, 6, -1]
+        kp = KernelPerceptron(2, poly=True, d=1)
+        o, val = kp.classify(example, train_data)
+
+        self.assertEqual(o, 1)
 
 
 if __name__ == '__main__':
